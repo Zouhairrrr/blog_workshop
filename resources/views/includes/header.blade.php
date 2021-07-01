d5ae87531a2fccd440313fbd049c94381e6a2fa2
Zouhairrrr <77061268+Zouhairrrr@users.noreply.github.com>
2021-07-01 09:35:35 +0100

    azer

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <title>Blog || web developement</title>
+
+</head>
+
+<body>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,15 @@
+@include('includes.header')
+<h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Pariatur voluptas illum nihil ducimus, explicabo voluptates assumenda distinctio eos dicta architecto maxime nobis!</h1>
+
+<div>
+    @yield('main_nav')
+</div>
+<div>
+    @yield('heading')
+</div>
+<div>
+    @yield('content')
+</div>
+
+
+@include('includes.footer')
